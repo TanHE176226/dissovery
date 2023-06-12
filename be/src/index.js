58f6da1d9d4f03f29b266bebebb865673f004f38
Ming <haminh0423@gmail.com>
2023-06-12 21:46:26 +0700

    update backend

--- be/src/index.js
+++ be/src/index.js
@@ -2,11 +2,68 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const foodRoutes = require('./routes/footRoutes');
+const bodyParser = require('body-parser');
+
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.post('/signup', async (req, res) => {
+  const { FirstName, LastName, Email, Password } = req.body;
+
+  try {
+    const createdAccount = await prisma.account.create({
+      data: {
+        FirstName,
+        LastName,
+        Email,
+        Password
+      }
+    });
+    // console.log(req.body);
+    res.json(createdAccount);
+  } catch (error) {
+    console.error(error);
+    res.json("Error");
+  }
+})
+// });
+// app.get('/login', (req, res) => {
+//   res.send("Hello World");
+// }) 
+
+app.post('/login', async (req, res) => {
+  const { Email, Password } = req.body;
+  const account = await prisma.account.findFirst({
+    where: {
+      Email,
+      Password: String(Password) // Chuyển đổi Password thành chuỗi
+    }
+  });
+
+
+  if (account != null) {
+    res.json(account);
+  } else {
+    // Kiểm tra các trường hợp nhập sai và trả về mã lỗi tương ứng
+    if (!Email && !Password) {
+      res.status(400).json({ error: 'Email and password are required' });
+    } else if (!Email) {
+      res.status(400).json({ error: 'Email is required' });
+    } else if (!Password) {
+      res.status(400).json({ error: 'Password is required' });
+    } else {
+      res.status(401).json({ error: 'Invalid email or password' });
+    }
+  }
+});
+const PORT = 3000;
 
 // app.get('/', function (req, res) {
 //   res.send('Hello world');
@@ -17,3 +74,4 @@ app.use('/getfood', foodRoutes);
 app.listen(3001, function () {
   console.log('Example app listening on port 3001npm!');
 });
+
--- fe/src/App.js
+++ fe/src/App.js
@@ -5,16 +5,21 @@ import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import ProductDetail from './pages/productdetails/ProductDetail';
 import Cart from './pages/cart/Cart';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Shop from './pages/shop/Shop';
+import Login from './Login';
+import Signup from './Signup';
+
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/productdetails" element={<ProductDetail/>}/>
-      <Route path="/shop" element={<Shop/>}/>
+      <Route path="/" element={<Login />} />
+
+      <Route path="/productdetails" element={<ProductDetail />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/shop" element={<Shop />} />
     </Routes>
   );
 }
