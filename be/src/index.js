8e65dac41780ac6e306bf94170f933a1c75a31f5
Mycroft180202 <nhathnhe163949@fpt.edu.vn>
2023-06-12 22:11:40 +0700

    sua be

--- be/src/index.js
+++ be/src/index.js
@@ -11,9 +11,31 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 // app.get('/', function (req, res) {
 //   res.send('Hello world');
 // });
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
 
 app.use('/getfood', foodRoutes);
 
+
+app.post('/Shop', async(req, res) => {
+  const food = await prisma.food.findMany()
+  console.log(food)
+  res.send(food);
+});
+
+app.get('/SearchById/:id', async(req,res) => {
+  const {id} = req.params; 
+ 
+  const foods = await prisma.food.findMany()
+  const filtedFoods = []
+  foods.forEach(food => { if (food.Name.includes(id)){
+    filtedFoods.push(food) 
+  }})
+  console.log(filtedFoods)
+    res.send(filtedFoods) 
+  })
+ 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001npm!');
 });
