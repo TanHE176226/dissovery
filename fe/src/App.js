1831ec70771defa417b83953581fcb544c366446
TanHE176226 <you@example.com>
2023-06-13 11:18:34 +0700

    add view cart

--- fe/src/App.js
+++ fe/src/App.js
@@ -5,17 +5,17 @@ import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import ProductDetail from './pages/productdetails/ProductDetail';
 import Cart from './pages/cart/Cart';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Shop from './pages/shop/Shop';
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/productdetails" element={<ProductDetail/>}/>
-      <Route path="/shop" element={<Shop/>}/>
-      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/productdetails" element={<ProductDetail />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/cart/:cartID" element={<Cart />} />
     </Routes>
   );
 }
