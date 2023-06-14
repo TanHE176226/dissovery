588e17492cc014eaa9c216be6028b7d11fd5d5ec
Nguyễn Hoàng <nguyenhoangxdxd@gmail.com>
2023-06-14 14:07:17 +0700

    Cấu hình lại BE

--- be/src/routes/footRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const foodController = require('../controller/foodController');
-
-// Route to get food by ID
-router.get('/:id', foodController.getFoodById);
-
-module.exports = router;
