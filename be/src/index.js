3f6a9a76cbc133262cd6b2bd05a4e1eb7891b6c9
Nguyễn Hoàng <nguyenhoangxdxd@gmail.com>
2023-06-14 14:16:10 +0700

    Thêm lại phần be

---
+++ be/src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const foodRoutes = require('./routes/footRoutes');
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+//định nghĩa các route cho ứng dụng của mình bằng cách sử dụng các phương thức HTTP như GET, POST, PUT, DELETE, vv:
+app.get('/', function (req, res) {
+  res.send('Hello world');
+});
+
+//sử dụng routes để đưa đường dẫn đúng controller xử lý
+app.use('/getfood', foodRoutes);
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001npm!');
+});
